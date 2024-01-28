a0a48a37ea9e4634d48ad893089fd31077549f0c
SurajBargaje <surajbargaje.scoe.mechanical@gmail.com>
2024-01-28 23:33:12 +0530

    test

---
+++ Practice/second/test.java
@@ -0,0 +1,20 @@
+public class test {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        greet();
+
+
+        
+
+        int temp = a;
+        a=b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+       
+
+
+    }
+    
+}
